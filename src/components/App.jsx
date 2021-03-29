c3d5052ef5acb1fe9a9c072f8f00a7c18c79a1c1
yangtzujou <63780958+yangtzujou@users.noreply.github.com>
2021-03-29 21:13:33 +0800

    Simplify functions

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,16 +2,7 @@ import React from "react";
 import Entry from "./Entry";
 import emojipedia from "../emojipedia";
 
-function createEntry(emojiTerm) {
-  return (
-    <Entry
-      key={emojiTerm.id}
-      emoji={emojiTerm.emoji}
-      name={emojiTerm.name}
-      meaning={emojiTerm.meaning}
-    />
-  );
-}
+
 
 function App() {
   return (
@@ -19,7 +10,16 @@ function App() {
       <h1>
         <span>emojipedia</span>
       </h1>
-      <dl className="dictionary">{emojipedia.map(createEntry)}</dl>
+      <dl className="dictionary">
+        {emojipedia.map(emojiTerm => 
+          <Entry
+            key={emojiTerm.id}
+            emoji={emojiTerm.emoji}
+            name={emojiTerm.name}
+            meaning={emojiTerm.meaning}
+          />
+        )}  
+    </dl>
     </div>
   );
 }
